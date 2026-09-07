00a50a2162c237a43d72c22f0aca04d5373d93e8
Guadalupe Fermani <guadalupefermani@gmail.com>
2026-09-06 23:37:26 -0300

    Initial Commit

---
+++ UserLocator/UserLocatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserLocatorApp.swift
+//  UserLocator
+//
+//  Created by Guadalupe  on 06/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct UserLocatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
